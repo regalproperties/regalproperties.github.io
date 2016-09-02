268e16d81932b9e2e7122ef3551507dc61b263a3
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-09-01 21:49:10 -0400

    Fix mailing address, add to front page

--- uncompiled/contact.php
+++ uncompiled/contact.php
@@ -28,7 +28,7 @@
 		<div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2" id="more">
 		Email us at <a href="mailto:info@regalpropertysl.com">info@regalpropertysl.com</a> or call us on <a href="tel:94773738943">+94 (77) 373 8943</a>.<br />
 		Visit our office at:<br />
-		<a href="https://www.google.ca/maps/place/RIL+Property+(Pvt.)+Ltd.+-+%22Parkland%22/@6.9170766,79.858768,19.12z/data=!4m13!1m7!3m6!1s0x3ae2596c8a38368f:0xb6224e128ac12191!2s33+Park+St,+Colombo+00200,+Sri+Lanka!3b1!8m2!3d6.916869!4d79.8589528!3m4!1s0x0:0x7b30c26e358d6c5a!8m2!3d6.9175079!4d79.8587491">Suite 122, Block D (Regus), Parkland<Br />
+		<a href="https://www.google.ca/maps/place/RIL+Property+(Pvt.)+Ltd.+-+%22Parkland%22/@6.9170766,79.858768,19.12z/data=!4m13!1m7!3m6!1s0x3ae2596c8a38368f:0xb6224e128ac12191!2s33+Park+St,+Colombo+00200,+Sri+Lanka!3b1!8m2!3d6.916869!4d79.8589528!3m4!1s0x0:0x7b30c26e358d6c5a!8m2!3d6.9175079!4d79.8587491">Suite 1222, Level 12, Block D (Regus), Parkland<Br />
 		No 33, Park Street, Colombo 02, Sri Lanka</a></div>
 	</section>
 
